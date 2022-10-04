c0705d188eee599cb209be3a9c528fae99749e60
Vonkuro <vonkuro@gmail.com>
2022-10-04 12:24:30 +0200

    Ajout du logo et mise en colonne bootstrap

---
+++ TableauDeBord/dashboard.php
@@ -0,0 +1,48 @@
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Tableau de bord</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row align-items-start">
+            <div class="col">
+               <img src="../logo.jpg" alt="Eausiris">
+                Menu
+            </div>
+            <div class="col">
+                Info Prix
+                <p>
+                    Prix de l'eau
+                </p>
+                <p>
+                    Volume d'eau utilisée
+                </p>
+                <p>
+                    Facture d'eau effective
+                </p>
+                <p>
+                    Econome effective
+                </p>
+                <p>
+                    Facture d'eau effective
+                </p>
+                <p>
+                    Econome effective
+                </p>
+            </div>
+            <div class="col">
+                Graph Prix
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+<?php ?>
