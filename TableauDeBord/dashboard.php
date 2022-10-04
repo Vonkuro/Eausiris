4a45d95181190ffdf49df7b5c7a794ead70ae194
Vonkuro <vonkuro@gmail.com>
2022-10-04 13:46:16 +0200

    Ajout d'un vendor : phpchartLite, mise en page statique du dashboard

--- TableauDeBord/dashboard.php
+++ TableauDeBord/dashboard.php
@@ -1,4 +1,6 @@
-
+<?php 
+    require_once("../vendor/phpChart_Lite/phpChart_Lite/conf.php");
+?>
 
 
 <!DOCTYPE html>
@@ -7,38 +9,55 @@
   <meta charset="utf-8">
   <title>Tableau de bord</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="./dashboard.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     <div class="container">
+        
+    <img src="../logo.jpg" alt="Eausiris">
         <div class="row align-items-start">
             <div class="col">
-               <img src="../logo.jpg" alt="Eausiris">
-                Menu
+                
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Irrigation</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Méthanisation</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Récolte</a>
+                    </li>
+                </ul>
             </div>
+
             <div class="col">
-                Info Prix
                 <p>
-                    Prix de l'eau
+                    Prix de l'eau <!-- Mois --> <span class="rectangle">0.035<!-- calcul --></span>
                 </p>
                 <p>
-                    Volume d'eau utilisée
+                    Volume d'eau utilisée <!-- Mois --> <span class="rectangle">0.035<!-- calcul --></span>
                 </p>
                 <p>
-                    Facture d'eau effective
+                    Facture d'eau effective <!-- Mois --> <span class="rectangle">0.035<!-- calcul --></span>
                 </p>
                 <p>
-                    Econome effective
+                    Econome effective <!-- Mois --> <span class="rectangle">0.035<!-- calcul --></span>
                 </p>
                 <p>
-                    Facture d'eau effective
+                    Facture d'eau effective <!-- Mois --> <span class="rectangle">0.035<!-- calcul --></span>
                 </p>
                 <p>
-                    Econome effective
+                    Econome effective <!-- Mois --> <span class="rectangle">0.035<!-- calcul --></span>
                 </p>
             </div>
+
             <div class="col">
-                Graph Prix
+                <?php
+                    $pc = new C_PhpChartX(array(array(11, 9, 5, 12, 14)),'basic_chart');
+                    $pc->draw();
+                ?>
             </div>
         </div>
     </div>
