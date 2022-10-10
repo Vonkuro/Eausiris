1321116baec3341af8c1e9467d7832c32b0bd158
JRedxs <98217327+JRedxs@users.noreply.github.com>
2022-10-10 18:17:23 +0200

    mise à jour et amélioration du menu

--- Login/login.php
+++ Login/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('../Workspace/modele.php');
+include('modele.php');
 
 ?>
 
@@ -15,14 +15,32 @@ include('../Workspace/modele.php');
   ?>
   <link rel="stylesheet" href="login.css">
 </head>
+<body>
+<!-- <div class="CarouselBackground">
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" style="height:1024px; width:1920px; z-index: 1;" src="./agroalimentaire.jpg" alt="First slide">
+    </div> -->
 
-<div class="login-page">
-    <div class="form">
+<div class="login-page" style="z-index: index 2;;">
+    <div class="form" style=" border: 2mm ridge rgba(125, 0, 255, .6);">
+    <img src="./logo.png" class="mx-auto d-block" alt="Eausiris">
+    <h1 class="font-italic" class="mx-auto d-block"> Bougez Moins !</h1>
+    <h2 class="font-italic" class="mx-auto d-block"> Economisez Plus !</h2>
       <form class="login-form" action="./login.php" method="post">
         <input type="text" name="identifiant" placeholder="pseudo" value="<?php $identifiant ?>"required/>
         <input type="password" name="motdepasse" placeholder="mot de passe" value="<?php $motdepasse ?>" required/>
         <button  type="submit" <a href="dashboard.php">Connexion</a></button>
-        <p class="message">Pas encore enregistrée? <a href="../CreateCompte/createCompte.php">Créez un compte!</a></p>
+        <p class="message">Pas encore enregistrée? <a href="createCompte.php">Créez un compte!</a></p>
       </form>
+
     </div>
+    
+<!-- </div>
+</div>
+  </div> -->
   </div>
+</body>
+
+  
